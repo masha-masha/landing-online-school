3588a2afedbbfd5e6916d32b278800ae9877809a
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-09-08 22:48:32 +0300

    temporary component removed, components from components folder moved to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import Temporary from "./temporary"
+import Header from './components/header/Header';
+import Intro from './components/intro/Intro';
+import Skills from './components/skills/Skills';
+import AboutUs from './components/about/AboutUs';
+import Pricing from './components/pricing/Pricing';
+
 
 function App() {
-
-  return (
-    <Temporary/>
-  )
+ return (
+  <>
+   <body>
+    <Header />
+    <main>
+     <Intro />
+     <Skills />
+     <AboutUs />
+     <Pricing />
+    </main>
+    <footer></footer>
+   </body>
+  </>
+ );
 }
 
-export default App
+export default App;
